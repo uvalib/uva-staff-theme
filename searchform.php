db3d583cc9ad0492001c61198486d170a40a0a49
Doug Chestnut <dougchestnut@gmail.com>
2015-11-03 10:53:02 -0500

    working on search ux-547

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form>
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url('/'); ?>">
   <div class="input-field">
-    <input id="search" type="search" required>
+    <input id="search" type="search" required value="<?php echo get_search_query(); ?>" name="s">
     <label for="search"><i class="mdi-action-search"></i></label>
     <i class="mdi-navigation-close"></i>
   </div>
