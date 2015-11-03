c472b8a5c553481093e1a24b05a058bc0a3cadc0
Starrie Williamson <starrie@virginia.edu>
2015-11-03 14:20:34 -0500

    Update search.php
    Had to switch around some of the divs because the sidebar is not positioned correctly.

--- search.php
+++ search.php
@@ -7,9 +7,9 @@
  * @package uvalibrarystaff
  */
 
+
+
 get_header(); ?>
-<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
 
  <div class="container content">
@@ -20,6 +20,11 @@ get_header(); ?>
 
       <div class="col s8"> 
         <div>
+
+
+ 	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
 
 		<?php if ( have_posts() ) : ?>
 
