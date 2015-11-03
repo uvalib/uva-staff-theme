4d101f7a0acce0fd68e04ba28a4f3cea33a9310c
Doug Chestnut <dougchestnut@gmail.com>
2015-11-03 10:44:07 -0500

    working on search ux-547

--- header.php
+++ header.php
@@ -54,13 +54,7 @@
             </div>
           </div>
           <div class="col m12 l4">
-                  <form>
-                    <div class="input-field">
-                      <input id="search" type="search" required>
-                      <label for="search"><i class="mdi-action-search"></i></label>
-                      <i class="mdi-navigation-close"></i>
-                    </div>
-                  </form>
+                  <?php get_search_form(); ?>
            </div>
     </div>
   
