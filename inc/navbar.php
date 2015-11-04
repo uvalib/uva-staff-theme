4af454b106867c81f7274fc22297b280e0afd591
ssw3r <starrie@virginia.edu>
2015-11-04 16:40:49 -0500

    alphabetize mega menu

--- inc/navbar.php
+++ inc/navbar.php
@@ -30,11 +30,11 @@
 
                              <h3 class="noLinkHeader">General</h3>                   
                         <ul>
+                            <li><a href="<?php bloginfo(''); ?>/category/library-leadership-notes/"><h4>Library Leadership Notes</h3></a></li>
+                            <li><a href="<?php bloginfo(''); ?>/library_strategy/"><h4>Library Strategy</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/organizational-design/organizational-values/"><h4>Organizational Values</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/organizational-design/staff-resources-for-organizational-change/"><h4>Staff Resources for Organizational Change</h4></a></li>
-                            <li><a href="<?php bloginfo(''); ?>/library_strategy/"><h4>Library Strategy</h4></a></li>
                               <li><a href="<?php bloginfo(''); ?>/category/town-meeting-notes/"><h4>Town Meeting Notes</h3></a></li>
-                            <li><a href="<?php bloginfo(''); ?>/category/library-leadership-notes/"><h4>Library Leadership Notes</h3></a></li>
                         </ul>
                     </div>
 
@@ -43,8 +43,8 @@
                         <ul>
                             <li><a href="<?php bloginfo(''); ?>/"><h4>Ergonomics Team</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/groups/expert-testers/"><h4>Expert Testers Group</h4></a></li>
-                            <li><a href="<?php bloginfo(''); ?>/"><h4>Lib-EP Community</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/"><h4>GreenLib Community</h4></a></li>
+                            <li><a href="<?php bloginfo(''); ?>/"><h4>Lib-EP Community</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/"><h4>Professional development working group</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/groups/professional-interests-committee/"><h4>Professional Interests Committee (PIC)</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/"><h4>User Experience Community</h4></a></li>
@@ -66,38 +66,35 @@
                     <div class="nav-column">
                         <h3 class="noLinkHeader">Computing</h3>
                         <ul>
-                            <li><a href="<?php bloginfo(''); ?>/category/staff-computing-technology/"><h4>Staff Computing &amp; Technology</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/"><h4>Logins</h4></a>
                                 <ul>
                                     <li><a href="<?php bloginfo(''); ?>/"><h5>Director's Station</h5></a></li>
                                     <li><a href="http://analytics.lib.virginia.edu"><h5>Piwik</h5></a></li>
                                     <li><a href="https://ref.lib.virginia.edu"><h5>LibGuides (or LibApps)</h5></a></li>
                                 </ul>
-
-
-
                                     </li>
                             <li><a href="https://ref.lib.virginia.edu"><h4>Public Services Wiki</h4></a></li>
+                                <li><a href="<?php bloginfo(''); ?>/category/staff-computing-technology/"><h4>Staff Computing &amp; Technology</h4></a></li>
                         </ul>
                     </div>
                      
                     <div class="nav-column">
                         <h3 class="noLinkHeader">Library Human Resources</h3>
                         <ul>
-                            <li><a href="<?php bloginfo(''); ?>/"><h4>Tax Forms</h4></a></li>
+                            <li><a href="http://www.hr.virginia.edu/hr-for-you/classified-staff/classified-staff-policies-procedures/"><h4>Classified Staff Policies</h4></a></li>
+                            <li><a href="<?php bloginfo(''); ?>/"><h4>Faculty promotion guidelines &amp; review</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/category/open-positions-selections/"><h4>Open Positions</h4></a></li>
                             <li><a href="http://www.hr.virginia.edu/hr-for-you/managers/performance-management/performance-process/"><h4>Performance Review Information</h4></a></li>
-                            <li><a href="<?php bloginfo(''); ?>/"><h4>Faculty promotion guidelines &amp; review</h4></a></li>
+                            <li><a href="<?php bloginfo(''); ?>/"><h4>Tax Forms</h4></a></li>
                             <li><a href="http://provost.virginia.edu/faculty-resources/policies"><h4>University Faculty Policies</h4></a></li>
-                            <li><a href="http://www.hr.virginia.edu/hr-for-you/classified-staff/classified-staff-policies-procedures/"><h4>Classified Staff Policies</h4></a></li>
                             <li><a href="<http://www.hr.virginia.edu/hr-for-you/university-staff/university-staff-policies-and-procedures/"><h4>University Staff Policies</h4></a></li>
                         </ul>
                     </div>
                      <div class="nav-column">
                         <h3 class="noLinkHeader">Professional Development</h3>
                         <ul>
-                            <li><a href="http://staff.lib.virginia.edu/HR/training/calendar.html"><h4>Learning Calendar</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/hr-budget-grants/leadership-programs-for-academic-librarians/"><h4>Leadership programs for librarians</h4></a></li>
+                            <li><a href="http://staff.lib.virginia.edu/HR/training/calendar.html"><h4>Learning Calendar</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/"><h4>Professional development Working Group</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/policies/professional-development-funding-program/"><h4>Professional Development Funding Program</h4></a></li>
                             <li><a href="<?php bloginfo(''); ?>/professional-interests-committee/"><h4>Professional Interests Committee (PIC)</h4></a></li>
