e11a8fe2d67f8106aab1ed6336dd657be5820f78
ssw3r <starrie@virginia.edu>
2015-11-13 13:43:16 -0500

    fixed the css code showing up on the Library Experience page

--- js/init.js
+++ js/init.js
@@ -11,5 +11,17 @@
       $('.slider').slider({full_width: true});
 
   }); // end of document ready
+
+
+
+function toggleDiv(divId) {
+$(‘.members’).fadeOut(‘fast’);
+$(“#”+divId).slideToggle(‘slow’);
+}
+
+  
 })(jQuery); // end of jQuery name space
+
+
+
 
