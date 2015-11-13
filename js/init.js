e70c785d87bb2b8618247f91bd932fd5ca97b9de
Jack Kelly <jlk4p@virginia.edu>
2015-11-13 15:49:34 -0500

    add li.active to specify which body to display

--- js/init.js
+++ js/init.js
@@ -13,7 +13,7 @@
 		// make sure sidebar navigation active item appears.
 		if($('li').hasClass('active')) {
 			$('a.collapsible-header').addClass('active');
-			$('ul.collapsible-body').css('display','block');
+			$('li.active > ul.collapsible-body').css('display','block');
 		}
   }); // end of document ready
 })(jQuery); // end of jQuery name space
