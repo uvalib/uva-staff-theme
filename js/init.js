2a36a55f2195fa560b9e6324cc585d0e02c0e097
Jack Kelly <jlk4p@virginia.edu>
2015-11-13 16:03:21 -0500

    add parent selector for testing when identifying current sidebar nav selecition

--- js/init.js
+++ js/init.js
@@ -12,8 +12,8 @@
 
 		// make sure sidebar navigation active item appears.
 		if($('li').hasClass('active')) {
-			$('a.collapsible-header').addClass('active');
-			$('li.active > ul.collapsible-body').css('display','block');
+			$('li.active').parents('a.collapsible-header').addClass('active');
+			$('li.active').parents('ul.collapsible-body').css('display','block');
 		}
   }); // end of document ready
 })(jQuery); // end of jQuery name space
