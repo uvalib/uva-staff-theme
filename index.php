490d36defb50f2affe689db6393ca5b9e8b6880f
ssw3r <starrie@virginia.edu>
2015-12-07 16:43:27 -0500

    added the library leadership blog feed code that Tony wrote for the home page

--- index.php
+++ index.php
@@ -82,32 +82,19 @@ get_header(); ?>
 
 <!-- Here is the code to pull in the Library Leadership Notes items on the current Staff website home page - Starrie -->
 
-<?php // Get RSS Feed(s)
-include_once( ABSPATH . WPINC . '/feed.php' );
-// Get a SimplePie feed object from the specified feed source.
-$rss = fetch_feed( 'http://leadership.library.virginia.edu/feed/' );
-if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
-    // Figure out how many total items there are, but limit it to 5.
-    $maxitems = $rss->get_item_quantity( 5 );
-    // Build an array of all the items, starting with element 0 (first element).
-    $rss_items = $rss->get_items( 0, $maxitems );
-endif;
-?>
-<ul>
-    <?php if ( $maxitems == 0 ) : ?>
-        <li><?php _e( 'No items', 'my-text-domain' ); ?></li>
-    <?php else : ?>
-        <?php // Loop through each feed item and display each item as a hyperlink. ?>
-        <?php foreach ( $rss_items as $item ) : ?>
-            <li>
-                <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
-                    title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
-                    <?php echo esc_html( $item->get_title() ); ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    <?php endif; ?>
-</ul>
+ <h5 class="center">Leadership Blog</h5>
+              </div>
+                <?php
+                wprss_display_feed_items( $args = array(
+                    'links_before' => '<ul class="collection">',
+                    'links_after' => '</ul>',
+                    'link_before' => '<li class="collection-item">',
+                    'link_after' => '</li>',
+                    'limit' => '3',
+                    'source' => '8170'
+                    ));
+                ?>
+            </div>
 
 
 
