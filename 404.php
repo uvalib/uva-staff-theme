33f06461f9ed6e54aee33d9ffc1fd2073aabdcbe
ssw3r <starrie@virginia.edu>
2015-12-29 15:19:42 -0500

    fix 404 pages

--- 404.php
+++ 404.php
@@ -6,11 +6,21 @@
  *
  * @package uvalibrarystaff
  */
-
 get_header(); ?>
+
+ <div class="container content">
+
+
+    <div class="section">
+ <div class="row">
+
+      <div class="col s8"> 
+        <div>
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+
 
 			<section class="error-404 not-found">
 				<header class="page-header">
@@ -49,10 +59,52 @@ get_header(); ?>
 
 					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
+				</main><!-- #main -->
 	</div><!-- #primary -->
+		</div><!--end empty div (is this even needed?)-->
+
+    </div><!--end col s8 -->
+
+  <div class="col s4" id="sideNav">
+		    			<?php get_sidebar(); ?>
+
+
+
+<!-- Here is the code to pull in the Library Leadership Notes items on the current Staff website home page - Starrie -->
+
+<div class="widget widget_leadershipBlog">
+ <h2 class="widgettitle"><a href="http://leadership.library.virginia.edu">Library Leadership Blog</a></h2>
+                <?php
+                wprss_display_feed_items( $args = array(
+                    'links_before' => '<ul class="collection">',
+                    'links_after' => '</ul>',
+                    'link_before' => '<li class="collection-item">',
+                    'link_after' => '</li>',
+                    'limit' => '3',
+                    'source' => '8170'
+                    ));
+                ?>
+
+
+              <a class="btn-large waves-effect waves-light orange darken-1" href="http://leadership.library.virginia.edu">Read More</a>
+
+
+            </div>
+
+
+
+
+
+		    			<?php the_widget( 'WP_Widget_Categories', $instance, $args ); ?> 
+		    			<?php the_widget( 'WP_Widget_Tag_Cloud', $instance, $args ); ?> 
+		    			<?php the_widget( 'WP_Widget_Archives', $instance, $args ); ?> 
+
+		    		</div>
+		    			
+  </div>
+  </div>
+</div>
+
+
 
 <?php get_footer(); ?>
