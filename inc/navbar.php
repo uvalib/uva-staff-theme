5825caa9c6405dc427d7e35c98d7d82d7cfbec40
Renee Reighart <reneereighart@gmail.com>
2016-01-26 12:53:03 -0500

    Added LibAnalytics to logins

--- inc/navbar.php
+++ inc/navbar.php
@@ -56,10 +56,11 @@
                         <ul>
                             <li><a href=""><h4>Logins</h4></a>
                                 <ul>
+                                    <li><a href="https://libguides.com/login.php?iid=216" class="externalLink"><h5>LibGuides</h5></a></li>
+                                    <li><a href="https://virginia.libanalytics.com/" class="externalLink"><h5>LibAnalytics</h5></a></li>
+                                    <li><a href="https://virginia.libapps.com" class="externalLink"><h5>LibApps</h5></a></li>
                                     <li><a href="http://lb-mssql.eservices.virginia.edu/directorsstation/" class="externalLink"><h5>Director's Station</h5></a></li>
                                     <li><a href="http://analytics.lib.virginia.edu" class="externalLink"><h5>Piwik</h5></a></li>
-                                    <li><a href="https://libguides.com/login.php?iid=216" class="externalLink"><h5>LibGuides</h5></a></li>
-                                    <li><a href="https://virginia.libapps.com" class="externalLink"><h5>LibApps</h5></a></li>
                                 </ul>
                                     </li>
                             <li><a href="https://ref.lib.virginia.edu" class="externalLink"><h4>Public Services Wiki</h4></a></li>
