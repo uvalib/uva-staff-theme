9d82a5b0db632ce6bbaf5f3080d692d02d113b2a
tony-jones <ad30jone@gmail.com>
2016-10-10 16:06:12 -0400

    UX-770 library leadership not updating

--- page-category.php
+++ page-category.php
@@ -15,7 +15,7 @@ get_header(); ?>
     <div class="section">
  <div class="row">
 
-      <div class="col l8 m12"> 
+      <div class="col l8 m12">
         <div>
 
 
@@ -25,23 +25,23 @@ get_header(); ?>
 			<section>
 				<?php the_post(); ?>
 				<?php the_title( '<h1 class="section-title">', '</h1>' ); ?>
-				
+
 				<div class="page-content">
 					<?php the_content(); ?>
 				</div><!-- .page-content -->
-				
-				<?php 
+
+				<?php
 					/* Lets see if we have a category to display */
 					global $post;
-					
+
 					// Save our page posts to use later
 					$tmp_post = $post;
 					$catids[] = array();
-					
+
 					// Do we have categories set on the custom fields
 					$meta_cats = get_post_meta($post->ID, 'categories', true);
-					
-					// Do not rely on what the admin entered 
+
+					// Do not rely on what the admin entered
 					if( $meta_cats ) {
 						$categories = explode(',', $meta_cats);
 						foreach ($categories as $meta_cat) {
@@ -49,28 +49,28 @@ get_header(); ?>
 							$catids[] = is_numeric( $meta_cat ) ? $meta_cat : get_cat_ID(trim($meta_cat));
 						}
 					}
-					
+
 					// No category ID so use the page slug to find a category
 					if( count($catids) == 0 ) {
-						$catid = get_cat_ID($post->post_name);   
+						$catid = get_cat_ID($post->post_name);
 						if( $catid && is_numeric( $catid ) ) $catids[] = $catid;
 					}
-					
+
 					// If we have a category id we can get the category posts
 					if( count($catids) != 0 ) {
 						$do_not_show_stickies = 1; // 0 to show stickies
 						$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-						$args = array( 
+						$args = array(
 							'cat' => implode(',', $catids),
 							'post_type' => array('post'),
 							'order' => 'DESC',
 							'paged' => $paged,
 							'ignore_sticky_posts' => $do_not_show_stickies
 						);
-						
+
 						$wp_query = null;
 						$wp_query = new WP_Query( $args );
-							
+
 						// Output our Query
 						if ( $wp_query->have_posts() ) {
 							while ( $wp_query->have_posts() ) {
@@ -78,15 +78,15 @@ get_header(); ?>
 								get_template_part( 'template-parts/content', 'category-page' );
 							}
 						}
-					}	
+					}
 					wp_reset_postdata();
 				?>
-				
-				<?php 
+
+				<?php
 					// Reset the post to the page post
-					$post = $tmp_post; 
+					$post = $tmp_post;
 				?>
-				
+
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
@@ -116,7 +116,7 @@ get_header(); ?>
                     'link_before' => '<li class="collection-item">',
                     'link_after' => '</li>',
                     'limit' => '3',
-                    'source' => '8170'
+                    'source' => '8543'
                     ));
                 ?>
 
@@ -126,12 +126,12 @@ get_header(); ?>
 
             </div>
 
-						<?php the_widget( 'WP_Widget_Categories', $instance, $args ); ?> 
-						<?php the_widget( 'WP_Widget_Tag_Cloud', $instance, $args ); ?> 
-						<?php the_widget( 'WP_Widget_Archives', $instance, $args ); ?> 
+						<?php the_widget( 'WP_Widget_Categories', $instance, $args ); ?>
+						<?php the_widget( 'WP_Widget_Tag_Cloud', $instance, $args ); ?>
+						<?php the_widget( 'WP_Widget_Archives', $instance, $args ); ?>
 
 					</div>
-		    			
+
   </div>
   </div>
 </div>
